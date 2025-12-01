09ed24a87e478f384de043833555b1502284ee50
Safonov Zakhar <safonov.zakhar.05@mail.ru>
2025-12-02 01:01:57 +0700

    В папку UI добавил код интерфейса

---
+++ seismictools/apps/FK_analysis/UI/SettingsWidget_ui.py
@@ -0,0 +1,146 @@
+# -*- coding: utf-8 -*-
+
+################################################################################
+## Form generated from reading UI file 'SettingsWidget.ui'
+##
+## Created by: Qt User Interface Compiler version 6.10.1
+##
+## WARNING! All changes made in this file will be lost when recompiling UI file!
+################################################################################
+
+from PySide6.QtCore import (QCoreApplication, QDate, QDateTime, QLocale,
+    QMetaObject, QObject, QPoint, QRect,
+    QSize, QTime, QUrl, Qt)
+from PySide6.QtGui import (QBrush, QColor, QConicalGradient, QCursor,
+    QFont, QFontDatabase, QGradient, QIcon,
+    QImage, QKeySequence, QLinearGradient, QPainter,
+    QPalette, QPixmap, QRadialGradient, QTransform)
+from PySide6.QtWidgets import (QApplication, QGroupBox, QHBoxLayout, QLabel,
+    QListWidget, QListWidgetItem, QMainWindow, QPushButton,
+    QSizePolicy, QStatusBar, QVBoxLayout, QWidget)
+
+from pyqtgraph import PlotWidget
+
+class Ui_MainWindow(object):
+    def setupUi(self, MainWindow):
+        if not MainWindow.objectName():
+            MainWindow.setObjectName(u"MainWindow")
+        MainWindow.resize(1151, 605)
+        self.centralwidget = QWidget(MainWindow)
+        self.centralwidget.setObjectName(u"centralwidget")
+        self.horizontalLayout = QHBoxLayout(self.centralwidget)
+        self.horizontalLayout.setObjectName(u"horizontalLayout")
+        self.ToolGB = QGroupBox(self.centralwidget)
+        self.ToolGB.setObjectName(u"ToolGB")
+        sizePolicy = QSizePolicy(QSizePolicy.Policy.Fixed, QSizePolicy.Policy.Preferred)
+        sizePolicy.setHorizontalStretch(0)
+        sizePolicy.setVerticalStretch(0)
+        sizePolicy.setHeightForWidth(self.ToolGB.sizePolicy().hasHeightForWidth())
+        self.ToolGB.setSizePolicy(sizePolicy)
+        self.verticalLayout = QVBoxLayout(self.ToolGB)
+        self.verticalLayout.setObjectName(u"verticalLayout")
+        self.SeismicDataGB = QGroupBox(self.ToolGB)
+        self.SeismicDataGB.setObjectName(u"SeismicDataGB")
+        self.verticalLayout_2 = QVBoxLayout(self.SeismicDataGB)
+        self.verticalLayout_2.setObjectName(u"verticalLayout_2")
+        self.SeismicDataLW = QListWidget(self.SeismicDataGB)
+        self.SeismicDataLW.setObjectName(u"SeismicDataLW")
+
+        self.verticalLayout_2.addWidget(self.SeismicDataLW)
+
+        self.label = QLabel(self.SeismicDataGB)
+        self.label.setObjectName(u"label")
+
+        self.verticalLayout_2.addWidget(self.label)
+
+        self.SeismicDataBtn = QPushButton(self.SeismicDataGB)
+        self.SeismicDataBtn.setObjectName(u"SeismicDataBtn")
+
+        self.verticalLayout_2.addWidget(self.SeismicDataBtn)
+
+
+        self.verticalLayout.addWidget(self.SeismicDataGB)
+
+        self.SeismicStartGB = QGroupBox(self.ToolGB)
+        self.SeismicStartGB.setObjectName(u"SeismicStartGB")
+        self.verticalLayout_3 = QVBoxLayout(self.SeismicStartGB)
+        self.verticalLayout_3.setObjectName(u"verticalLayout_3")
+        self.FkBtn = QPushButton(self.SeismicStartGB)
+        self.FkBtn.setObjectName(u"FkBtn")
+
+        self.verticalLayout_3.addWidget(self.FkBtn)
+
+        self.SignalBtn = QPushButton(self.SeismicStartGB)
+        self.SignalBtn.setObjectName(u"SignalBtn")
+
+        self.verticalLayout_3.addWidget(self.SignalBtn)
+
+        self.ResultBtn = QPushButton(self.SeismicStartGB)
+        self.ResultBtn.setObjectName(u"ResultBtn")
+
+        self.verticalLayout_3.addWidget(self.ResultBtn)
+
+
+        self.verticalLayout.addWidget(self.SeismicStartGB)
+
+        self.ErrorGB = QGroupBox(self.ToolGB)
+        self.ErrorGB.setObjectName(u"ErrorGB")
+        self.verticalLayout_4 = QVBoxLayout(self.ErrorGB)
+        self.verticalLayout_4.setObjectName(u"verticalLayout_4")
+        self.listWidget = QListWidget(self.ErrorGB)
+        self.listWidget.setObjectName(u"listWidget")
+
+        self.verticalLayout_4.addWidget(self.listWidget)
+
+
+        self.verticalLayout.addWidget(self.ErrorGB)
+
+
+        self.horizontalLayout.addWidget(self.ToolGB)
+
+        self.groupBox = QGroupBox(self.centralwidget)
+        self.groupBox.setObjectName(u"groupBox")
+        self.verticalLayout_5 = QVBoxLayout(self.groupBox)
+        self.verticalLayout_5.setObjectName(u"verticalLayout_5")
+        self.SeismogramPW = PlotWidget(self.groupBox)
+        self.SeismogramPW.setObjectName(u"SeismogramPW")
+
+        self.verticalLayout_5.addWidget(self.SeismogramPW)
+
+        self.FkPW = PlotWidget(self.groupBox)
+        self.FkPW.setObjectName(u"FkPW")
+
+        self.verticalLayout_5.addWidget(self.FkPW)
+
+        self.ResultPW = PlotWidget(self.groupBox)
+        self.ResultPW.setObjectName(u"ResultPW")
+
+        self.verticalLayout_5.addWidget(self.ResultPW)
+
+
+        self.horizontalLayout.addWidget(self.groupBox)
+
+        MainWindow.setCentralWidget(self.centralwidget)
+        self.statusbar = QStatusBar(MainWindow)
+        self.statusbar.setObjectName(u"statusbar")
+        MainWindow.setStatusBar(self.statusbar)
+
+        self.retranslateUi(MainWindow)
+
+        QMetaObject.connectSlotsByName(MainWindow)
+    # setupUi
+
+    def retranslateUi(self, MainWindow):
+        MainWindow.setWindowTitle(QCoreApplication.translate("MainWindow", u"MainWindow", None))
+        self.ToolGB.setTitle(QCoreApplication.translate("MainWindow", u"\u041f\u0430\u043d\u0435\u043b\u044c \u0438\u043d\u0441\u0442\u0440\u0443\u043c\u0435\u043d\u0442\u043e\u0432", None))
+        self.SeismicDataGB.setTitle(QCoreApplication.translate("MainWindow", u"\u0417\u0430\u0433\u0440\u0443\u0437\u043a\u0430 \u0434\u0430\u043d\u043d\u044b\u0445", None))
+        self.label.setText(QCoreApplication.translate("MainWindow", u"\u0424\u043e\u0440\u043c\u0430\u0442: .sgy / .segy", None))
+        self.SeismicDataBtn.setText(QCoreApplication.translate("MainWindow", u"\u041e\u0431\u0437\u043e\u0440", None))
+        self.SeismicStartGB.setTitle(QCoreApplication.translate("MainWindow", u"FK - \u0424\u0418\u041b\u042c\u0422\u0420\u0410\u0426\u0418\u042f", None))
+        self.FkBtn.setText(QCoreApplication.translate("MainWindow", u"\u041f\u043e\u0441\u0442\u0440\u043e\u0438\u0442\u044c FK-\u0441\u043f\u0435\u043a\u0442\u0440", None))
+        self.SignalBtn.setText(QCoreApplication.translate("MainWindow", u"\u0412\u044b\u0434\u0435\u043b\u0438\u0442\u044c \u0446\u0435\u043b\u0435\u0432\u043e\u0439 \u0441\u0438\u0433\u043d\u0430\u043b", None))
+        self.ResultBtn.setText(QCoreApplication.translate("MainWindow", u"\u041e\u0431\u0440\u0430\u0442\u043d\u043e\u0435 FK-\u043f\u0440\u0435\u043e\u0431\u0440\u0430\u0437\u043e\u0432\u0430\u043d\u0438\u0435", None))
+        self.ErrorGB.setTitle(QCoreApplication.translate("MainWindow", u"\u041e\u043a\u043d\u043e \u0441\u043e\u0441\u0442\u043e\u044f\u043d\u0438\u044f", None))
+        self.groupBox.setTitle(QCoreApplication.translate("MainWindow", u"Seismic Filtering (FK)", None))
+    # retranslateUi
+
