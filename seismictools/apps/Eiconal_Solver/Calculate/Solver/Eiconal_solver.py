66fcaea0c3d29c87b0bc7f7c3254bf9000707e2e
Sergey Mkrtchyan <mkrtchyan.sergej@gmail.com>
2025-12-18 22:48:44 +0700

    Закончил работу над вычислениями, обернул их в jit.

--- seismictools/apps/Eiconal_Solver/Calculate/Solver/Eiconal_solver.py
+++ seismictools/apps/Eiconal_Solver/Calculate/Solver/Eiconal_solver.py
@@ -1,40 +1,66 @@
 import numpy as np
+from numba import jit
 
-class EiconalEquationSolving:
 
-    @staticmethod
-    def gradient_point(v_model: np.ndarray, x: float, z: float, delta: float) -> np.ndarray:
-        n_x = int(x//delta)
-        n_z = int(z//delta)
-        res_x = (v_model[n_z][n_x+1] - v_model[n_z][n_x]) / delta
-        res_z = (v_model[n_z+1][n_x] - v_model[n_z][n_x]) / delta
-        return np.array([res_x, res_z])
 
-    @staticmethod
-    def gradient_field(v_model: np.ndarray, delta: float) -> np.ndarray:
-        rows, cols = v_model.shape
-        res = np.zeros((rows-1, cols-1, 2))
-        for i in range(len(v_model) - 1):
-            for j in range(len(v_model[i]) - 1):
-                res[i][j] = EiconalEquationSolving.gradient_point(v_model=v_model, x=delta*j, z=delta*i, delta=delta)
-        return res
+@jit(nopython=True)
+def gradient_point(model: np.ndarray, x: float, z: float, delta: float) -> np.ndarray:
+    n_x = int(x // delta)
+    n_z = int(z // delta)
+    nz, nx = model.shape
 
-    @staticmethod
-    def trajectory_calculator(v_model: np.ndarray, x_0: float, z_0: float, theta: float, delta: float) -> np.ndarray:
-        slowness_grad_field = EiconalEquationSolving.gradient_field(v_model ** (-1))
+    if n_x == 0:
+        res_x = (model[n_z][n_x + 1] - model[n_z][n_x]) / delta
+    else:
+        res_x = (model[n_z][n_x + 1] - model[n_z][n_x - 1]) / (2 * delta)
 
-        n_x_0 = int(x_0 // delta)
-        n_z_0 = int(z_0 // delta)
-        p_0 = [np.cos(theta) / v_model[n_x_0][n_z_0], np.sin(theta) / v_model[n_x_0][n_z_0]]
+    if n_z == 0:
+        res_z = (model[n_z + 1][n_x] - model[n_z][n_x]) / delta
+    else:
+        res_z = (model[n_z + 1][n_x] - model[n_z - 1][n_x]) / (2 * delta)
 
-        trajectory = [[x_0, z_0]]
-        p = [p_0]
+    return np.array([res_x, res_z])
 
-        while True:
-            n_x = int(trajectory[-1][0] // delta)
-            n_z = int(trajectory[-1][1] // delta)
-            p.append([p[-1][0] + delta * slowness_grad_field[n_x][n_z], p[-1][1] + delta * slowness_grad_field[n_x][n_z]])
-            trajectory.append([trajectory[-1][0] + delta * v_model[n_x][n_z] * p[-1][0], trajectory[-1][1] + delta * v_model[n_x][n_z] * p[-1][1]])
 
-        trajectory = np.array(trajectory)
-        return trajectory
+@jit(nopython=True)
+def gradient_field(model: np.ndarray, delta: float) -> np.ndarray:
+    rows, cols = model.shape
+    res = np.zeros((rows-1, cols-1, 2))
+    for i in range(len(model) - 1):
+        for j in range(len(model[i]) - 1):
+            res[i][j] = gradient_point(model=model, x=delta*j, z=delta*i, delta=delta)
+    return res
+
+
+@jit(nopython=True)
+def trajectory_calculator(v_model: np.ndarray, x_0: float, z_0: float, theta: float, delta: float) -> np.ndarray:
+
+    slowness_grad_field = gradient_field(model=v_model**(-1), delta=delta)
+
+    n_x_0 = int(x_0 // delta)
+    n_z_0 = int(z_0 // delta)
+
+    p_0 = [np.cos(theta) / v_model[n_z_0][n_x_0], np.sin(theta) / v_model[n_z_0][n_x_0]]
+
+    trajectory = [[x_0, z_0]]
+    p = [p_0]
+
+    while True:
+        n_x = int(trajectory[-1][0] // delta)
+        n_z = int(trajectory[-1][1] // delta)
+
+        if  n_x >= slowness_grad_field.shape[1] or n_z >= slowness_grad_field.shape[0] or n_z < 0 or n_x < 0:
+            break
+
+        p.append([p[-1][0] + delta * slowness_grad_field[n_z][n_x][0], p[-1][1] + delta * slowness_grad_field[n_z][n_x][1]])
+
+        dx = delta * v_model[n_z][n_x] * p[-1][0]
+        dz = delta * v_model[n_z][n_x] * p[-1][1]
+        #if delta * v_model[n_z][n_x] * p[-1][0] <= delta/1000:
+            #dx = 0.0
+        #if delta * v_model[n_z][n_x] * p[-1][1] <= delta/1000:
+            #dz = 0.0
+        trajectory.append([trajectory[-1][0] + dx, trajectory[-1][1] + dz])
+
+    trajectory = np.array(trajectory)
+    return trajectory
