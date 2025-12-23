f74c1d53413a04e78a56ec74c696a2dac7f9af19
Safonov Zakhar <safonov.zakhar.05@mail.ru>
2025-12-23 10:52:03 +0700

    Удалил ненужный метод

--- seismictools/apps/FK_analysis/Controller/PolygonMaskWorker.py
+++ seismictools/apps/FK_analysis/Controller/PolygonMaskWorker.py
@@ -43,19 +43,3 @@ class PolygonMaskWorker(QRunnable):
             j = i
 
         return inside.reshape(h, w)
-
-    def _point_in_polygon(self, x, y, poly_x, poly_y):
-        n = len(poly_x)
-        inside = False
-        p1x, p1y = poly_x[0], poly_y[0]
-        for i in range(1, n + 1):
-            p2x, p2y = poly_x[i % n], poly_y[i % n]
-            if y > min(p1y, p2y):
-                if y <= max(p1y, p2y):
-                    if x <= max(p1x, p2x):
-                        if p1y != p2y:
-                            xinters = (y - p1y) * (p2x - p1x) / (p2y - p1y) + p1x
-                        if p1x == p2x or x <= xinters:
-                            inside = not inside
-            p1x, p1y = p2x, p2y
-        return inside
